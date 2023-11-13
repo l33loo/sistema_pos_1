1991c1ae25e02f21621d476a4354515509d45d03
Miguel Gil <2023102850@uac.pt>
2023-11-13 12:19:41 -0100

    verificação do form

---
+++ registo.php
@@ -0,0 +1,49 @@
+<html>
+<body>
+
+<?php
+
+session_start();
+
+$nome = $_POST ["Nome do Artigo"];
+$preço = $_POST["Preço Unitário"];
+$erro = 0;
+
+//Verifica se existem erros nos campos
+//verifica se existe erros no $nome
+
+if(empty($nome) or strstr ($nome, ' ' ) == FALSE)
+    echo "Insira um nome válido"; $erro = 1;
+
+//verifica se existem erros no $preço
+
+if (empty($preço) or (is_float($preço) or strstr ($preço, ' ' ) == FALSE))
+    echo "Insira um valor válido"; $erro = 1;
+
+//Verifica se existem erros no codigo de barras
+
+if (empty($codigoBarras) or strlen($codigoBarras != 13) or strstr ($codigoBarras, ' ' ) == FALSE)
+    echo "Insira um Código de Barras válido"; $erro = 1;
+
+//Verifica se existem erros 
+
+if($erro == 0)
+    echo "Todos os campos foram preenchidos corretamente!";
+
+//Abrir ficheiro
+$nomeFicheiro = "artigos.csv";
+if (file_exists($nomeFicheiro)) {
+    $ficheiro = fopen($nomeFicheiro, "r");
+} else {
+    return [];
+}
+
+
+
+?>
+
+
+uiuiu
+
+</body>
+</html>
