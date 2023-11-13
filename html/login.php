5920aad5b80e57f40f25c1c11f5e92fda23d9c21
Lila Karpowicz <lila@karpowicz.dev>
2023-11-13 16:38:34 -0100

    change file and filename logic

--- html/login.php
+++ html/login.php
@@ -1,3 +1,5 @@
+<?php include('../app/login.inc'); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,29 +11,16 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    <!--<?php if (!empty($mensagem) ) { echo '<h3 style="color: red">' . $mensagem . "</h3>"; } ?>-->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/index.php">PoS</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-    </nav>
+    <?php include('./components/nav.inc') ?>
     <div class="container">
         <div class="row">
             <div class="col">
                 <h1>Login</h1>
+                <?php if (!empty($mensagemErro) ) { ?>
+                  <div class="alert alert-danger" role="alert">
+                    <?php echo $mensagemErro ?>
+                  </div>
+                <?php } ?>
                 <form action="" method="post">
                     <div class="row">
                         <div class="col-4">
