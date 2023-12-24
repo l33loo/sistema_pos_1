4d2aed966a805ea37d1a806f4b6c911be3ec6c1b
Lila Karpowicz <lila@karpowicz.dev>
2023-12-24 15:00:00 -0100

    include reusable head

--- html/register.php
+++ html/register.php
@@ -1,22 +1,17 @@
 <?php
 require_once '../config/config.php';
+require_once SERVER_ROOT . '/html/components/head.inc.php';
+
+$pageTitle = 'Criação de Conta';
+
+// HTML
+echo getHeader($pageTitle);
+include(SERVER_ROOT . '/html/components/body_start.inc.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Criar Nova Conta De Gestão</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</head>
-<body>
-    <div class="container">
         <div class="row mt-3">
             <div class="col">
-                <h1>Criação de Conta</h1>
+                <h1><?php echo $pageTitle ?></h1>
             </div>
         </div>
         
