7f45169400a2f9655cce3638292ba4e5b83deea1
Miguel Gil <2023102850@uac.pt>
2023-12-27 14:51:42 -0100

    Conflitc fixed

--- app/inc/session.inc.php
+++ app/inc/session.inc.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
     if (empty($_SESSION['autenticado']) || $_SESSION['autenticado'] === false) {
+        
         header('Location: ' . APP_ROOT . '/html/login.php');
+
         exit;
     }
