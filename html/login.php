bafee7075b70019276c82d8076f82f8647e5ce3d
Miguel Gil <2023102850@uac.pt>
2023-12-27 15:06:11 -0100

    fixes

--- html/login.php
+++ html/login.php
@@ -10,29 +10,36 @@ $pageTitle = 'Login';
 
 // HTML
 echo getHeader($pageTitle);
-include($_SERVER['DOCUMENT_ROOT'] . ROOT . '/components/nav.inc.php');
 ?>
 
-<h1><?php echo $pageTitle; ?></h1>
-<?php if (!empty($mensagemErro)) { ?>
-    <div class="alert alert-danger" role="alert">
-        <?php echo $mensagemErro ?>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href=<?php echo APP_ROOT . '/'?>>PoS</a>
     </div>
-<?php } ?>
-<form action="" method="post">
+</nav>
+<div class="container">
     <div class="row">
-        <div class="col-4">
-            <div class="mb-3">
-                <label class="form-label" for="email">Email:</label>
-                <input class="form-control" type="email" name="email" id="email" placeholder="introduza o seu email" required>
-            </div>
-            <div class="mb-3">
-                <label class="form-label" for="password">Password:</label>
-                <input class="form-control" type="password" name="password" id="password" placeholder="escreva a sua password" required>
-            </div>
-        </div>
-    </div>
-    <input class="btn btn-primary mb-3" type="submit" value="Iniciar Sessão" name="login">
-</form>
+        <div class="col">
+            <h1><?php echo $pageTitle; ?></h1>
+            <?php if (!empty($mensagemErro)) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $mensagemErro ?>
+                </div>
+            <?php } ?>
+            <form action="" method="post">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="mb-3">
+                            <label class="form-label" for="email">Email:</label>
+                            <input class="form-control" type="email" name="email" id="email" placeholder="introduza o seu email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="password">Password:</label>
+                            <input class="form-control" type="password" name="password" id="password" placeholder="escreva a sua password" required>
+                        </div>
+                    </div>
+                </div>
+                <input class="btn btn-primary mb-3" type="submit" value="Iniciar Sessão" name="login">
+            </form>
 
 <?php include(SERVER_ROOT . '/html/components/body_end.inc.php'); ?>
