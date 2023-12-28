9826266e4d6ca815cc2b8698aea7e7db2827e6a5
Miguel Gil <2023102850@uac.pt>
2023-12-28 20:06:04 -0100

    last commit

--- app/criar_artigo.php
+++ app/criar_artigo.php
@@ -6,14 +6,17 @@ require_once SERVER_ROOT . '/app/inc/artigos.inc.php';
 
 //ler Artigos
 $artigos = lerArtigos();
+
 if(array_key_exists($_POST['nome'], $artigos))
 {
     echo "O nome do artigo já está a ser utilizado";
-} elseif(array_key_exists($_POST['barras'], $artigos))
+}
+if(array_key_exists($_POST['barras'], $artigos))
 {
     echo" O código de barras já se encontra associado";
-} else {
-    //Adicionar artigos
+}
+
+    //Adicionar artigosz
 
     $artigos = adicionarArtigo($artigos, $_POST['nome'], $_POST['preco'], $_POST['iva'], $_POST['barras']);
 
@@ -22,5 +25,4 @@ if(array_key_exists($_POST['nome'], $artigos))
 guardarArtigos($artigos);
 
 header('Location: ' . APP_ROOT . '/');
-}
 
