291e10afe7829f7baf85f9cfb7991971a72fbd5c
Lila Karpowicz <lila@karpowicz.dev>
2023-12-29 00:22:44 -0100

    improve nav styling

--- html/components/body_start.inc.php
+++ html/components/body_start.inc.php
@@ -1,25 +1,27 @@
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href=<?php echo APP_ROOT . '/'?>>PoS</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href=<?php echo APP_ROOT . '/html/artigos.php'?>>Artigos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href=<?php echo APP_ROOT . '/html/register.php'?>>Criar Conta</a>
-                </li>
-                <li class="nav-item">
-                    <a class="btn btn-secondary" href=<?php echo APP_ROOT . '/app/logout.php'?>>Logout</a>
-                </li>
-            </ul>
+<div class="container">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand px-3" href=<?php echo APP_ROOT . '/'?>>PoS</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href=<?php echo APP_ROOT . '/html/artigos.php'?>>Artigos</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href=<?php echo APP_ROOT . '/html/register.php'?>>Criar Conta</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="btn btn-secondary" href=<?php echo APP_ROOT . '/app/logout.php'?>>Logout</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
 <div class="container">
     <div class="row mt-3">
         <div class="col">
